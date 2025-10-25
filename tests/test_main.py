2d95230525e84e390d10bc21c3ba05feaceef218
martinc999 <martinc999@gmail.com>
2025-10-25 21:00:08 +0200

    repo cleanup

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-"""Tests for chrome_tabpage_mgnt."""
-import pytest
-from chrome_tabpage_mgnt.main import main
-
-def test_main(capsys):
-    """Test main function."""
-    main()
-    captured = capsys.readouterr()
-    assert "Hello from chrome_tabpage_mgnt!" in captured.out
